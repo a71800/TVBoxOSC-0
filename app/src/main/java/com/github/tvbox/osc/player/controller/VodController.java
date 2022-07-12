25d7b1986a6f1a4d3c2ae1c7ba0d1c51b449832e
Marcotso <marco_tso@hotmail.com>
2022-07-12 19:04:00 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -307,7 +307,7 @@ public class VodController extends BaseController {
                     int step = Hawk.get(HawkConfig.PLAY_TIME_STEP, 5);
                     int et = mPlayerConfig.getInt("et");
                     et += step;
-                    if (et > 60 * 10)
+                    if (et > 60 * 3)
                         et = 0;
                     mPlayerConfig.put("et", et);
                     updatePlayerCfgView();
